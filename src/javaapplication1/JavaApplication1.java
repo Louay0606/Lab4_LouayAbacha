e6b71632d661783195790b8549d694fe3617858c
6313901 <6313901@host.docker.internal>
2025-09-22 08:22:23 -0400

    test

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import javafx.application.Application;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 6313901
+ */
+public class JavaApplication1 extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage)  {
+        
+        TextField Days = new TextField();
+        TextField Airfare = new TextField();
+        TextField CarFees = new TextField();  
+        TextField Miles = new TextField();
+        TextField PkFees = new TextField();
+        TextField TaxiFees = new TextField();
+        TextField RegistrationFees = new TextField();
+        TextField Miles = new TextField();
+}
+}
+
